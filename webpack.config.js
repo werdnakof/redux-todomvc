e4b187cecba2a8435fbf8fc5ecb0cdee319c762f
Andrew Fok <andrew.fok@tinklabs.com>
2016-11-05 12:23:35 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css' // We add the css loader
+    }]
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Wire in the hot loading plugin
+  ]
+};
